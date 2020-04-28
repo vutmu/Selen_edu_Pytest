afed629b9fd6af29320978ac983a693311b8f842
vutmu <63611274+vutmu@users.noreply.github.com>
2020-04-29 02:39:06 +0300

    Update test_items.py
    Вариант реализации ассерта через вспомогательную функцию

--- test_items.py
+++ test_items.py
@@ -1,10 +1,14 @@
 
 
+def is_element_present(browser):
+    try:
+        browser.implicitly_wait(10)
+        browser.find_element_by_css_selector("#add_to_basket_form")
+        return True
+    except:
+        return False
+
 def test_basket(browser):
     link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    browser.implicitly_wait(10)
-    try:
-        browser.find_element_by_css_selector("#add_to_basket_form")
-    except:
-        assert 1==2, "корзинка не найдена"
+    assert is_element_present(browser)==True, "корзинка не найдена"
