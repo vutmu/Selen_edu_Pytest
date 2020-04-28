b08d95e5e418f103facb9c45f509315439a663ea
vutmu <63611274+vutmu@users.noreply.github.com>
2020-04-29 00:47:01 +0300

    Update test_items.py
    теперь, если тест не найдет нужный элемент, будет корректное сообщение "корзинка не найдена"

--- test_items.py
+++ test_items.py
@@ -4,4 +4,7 @@ def test_basket(browser):
     link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     browser.implicitly_wait(10)
-    assert browser.find_element_by_css_selector("#add_to_basket_form")
+    try:
+        browser.find_element_by_css_selector("#add_to_basket_form")
+    except:
+        assert 1==2, "корзинка не найдена"
