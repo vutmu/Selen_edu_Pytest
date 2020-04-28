b6822cb5ade9f05167801ec6df899caec2911eb9
vutmu <63611274+vutmu@users.noreply.github.com>
2020-04-28 20:18:14 +0300

    version 1.01
    Теперь можно задать любой язык браузера

--- conftest.py
+++ conftest.py
@@ -9,14 +9,13 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     language=request.config.getoption("language")
-    if language=="es":
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': language})
-        print("\nstart browser for test..")
-        browser = webdriver.Chrome(options=options)
-        yield browser
-        # ожидание чтобы визуально оценить что браузер открылся на испанском языке!
-        time.sleep(10)
-        print("\nquit browser..")
-        browser.quit()
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    # ожидание чтобы визуально оценить что браузер открылся на испанском языке!
+    time.sleep(10)
+    print("\nquit browser..")
+    browser.quit()
 
