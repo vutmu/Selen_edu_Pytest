223104382989b79042884a620ad4128f145a91f6
vutmu <63611274+vutmu@users.noreply.github.com>
2020-04-29 12:28:28 +0300

    version 1.04
    Это скорее всего самый правильный вариант))

--- test_items.py
+++ test_items.py
@@ -4,5 +4,4 @@ def test_basket(browser):
     link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     browser.implicitly_wait(10)
-    check=browser.find_element_by_css_selector("#wrong locator")
-    assert check, "корзинка не найдена"
+    assert len(browser.find_elements_by_css_selector("#add_to_basket_form1"))>0, "корзинка не найдена"
