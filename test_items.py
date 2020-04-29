4242f5e76069a8dcbaac05a93b9f30074105a4ed
vutmu <63611274+vutmu@users.noreply.github.com>
2020-04-29 11:58:39 +0300

    version 1.03
    Этот вариант выдает неправильный отчет об ошибке!

--- test_items.py
+++ test_items.py
@@ -1,14 +1,8 @@
 
 
-def is_element_present(browser):
-    try:
-        browser.implicitly_wait(10)
-        browser.find_element_by_css_selector("#add_to_basket_form")
-        return True
-    except:
-        return False
-
 def test_basket(browser):
     link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    assert is_element_present(browser)==True, "корзинка не найдена"
+    browser.implicitly_wait(10)
+    check=browser.find_element_by_css_selector("#wrong locator")
+    assert check, "корзинка не найдена"
