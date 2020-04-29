5fd369b70de63fff6d7a1c146842ba8c8bd31939
vutmu <63611274+vutmu@users.noreply.github.com>
2020-04-29 15:44:58 +0300

    1.05
    fix locator

--- test_items.py
+++ test_items.py
@@ -4,4 +4,4 @@ def test_basket(browser):
     link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     browser.implicitly_wait(10)
-    assert len(browser.find_elements_by_css_selector("#add_to_basket_form1"))>0, "корзинка не найдена"
+    assert len(browser.find_elements_by_css_selector("#add_to_basket_form"))>0, "корзинка не найдена"
